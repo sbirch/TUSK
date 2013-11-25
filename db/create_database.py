8dcc8a2fedfc49acc078eb24eab2f0a338aadc1c
Sam Birch <sam.m.birch@gmail.com>
2013-11-25 00:31:22 -0500

    create db with iterators

--- db/create_database.py
+++ db/create_database.py
@@ -6,91 +6,91 @@ import zipfile
 import os
 import sqlite3
 import time
+import itertools
+import sys
+from collections import defaultdict
 
+def declare_table(cur, column_types, table_name):
+	cur.execute('DROP TABLE IF EXISTS %s' % table_name)
 
-parser = argparse.ArgumentParser(description='Locate input files')
-parser.add_argument('-z', '--zipfile-location', dest='path',
-	default='~/Dropbox/ATUS-195W/data/test_zip/',
-	help='The location of the zipfiles with the ATUS data')
-args = parser.parse_args()
-arg_dict = vars(args)
-path = arg_dict['path']
+	# Build the main table
+	colspec = ', '.join([name + ' ' + data_type for name,data_type in column_types.items()])
+	command = 'CREATE TABLE %s (%s)' % (table_name, colspec)
+	cur.execute(command)
 
-datafiles = os.listdir(path)
-
-conn = sqlite3.connect('atus.db')
-cur = conn.cursor()
-
-def find_dat_files(list_of_files):
-	correct_list = []
-	for x in range(len(list_of_files)):
-		last = list_of_files[x].split('/')[-1:][0]
-		if (len(last) > 0 and last[0] != '.' and last[-4:] == '.dat'):
-			correct_list.append(list_of_files[x])
-	return correct_list
-
-
-
-
-def parse_data(rawData):
-	colsLabels = {}
-	for i,x in enumerate(rawData[1]):
-		try:
-			time.strptime(x, '%H:%M:%S')
-			info_tuple = (rawData[0][i], 'time')
-			colsLabels[i] = info_tuple
-		except:
-			try:
-				float(x)
-				info_tuple = (rawData[0][i], 'float')
-				colsLabels[i] = info_tuple
-			except ValueError:
-				try:
-					str(x)
-					info_tuple = (rawData[0][i], 'string')
-					colsLabels[i] = info_tuple
-				except:
-					print'error'
-	return colsLabels
-
-def create_table_string(cols, name):
-	createString = 'CREATE TABLE ' + name + ' ('
-	for x in range(len(cols.keys())):
-		the_name, the_type = cols[x]
-		createString = createString + the_name + ' ' + the_type + ', '
-	createString = createString[:-2] + ');'
-	return createString
-
-def create_table(create_string, table_name):
-	cur.execute(create_string)
+	# Build an index on the case ID
 	index_string = 'CREATE INDEX TUCASEID_INDEX_%s ON %s (`TUCASEID` ASC);' % (table_name, table_name)
 	cur.execute(index_string)
 
-def insert_data(data_list, table_name):
-	columns = data_list.pop(0)
-	insert_string = 'INSERT INTO %s (%s) VALUES (%s);' % (
+def sniff_type(strings):
+	if all([x.isdigit() or (x[0]=='-' and x[1:].isdigit()) for x in strings]):
+		return 'INTEGER'
+	try:
+		[float(x) for x in strings]
+		return 'REAL'
+	except ValueError:
+		return 'TEXT'
+
+def build_table(cur, conn, table_name, csv_reader):
+	columns = csv_reader.next()
+	
+	# Pull off the first 100 records to sniff the column types
+	sniffed_data = []
+	sniff = defaultdict(list)
+	for line in csv_reader:
+		sniffed_data.append(line)
+		for i,value in enumerate(line):
+			sniff[columns[i]].append(value)
+		if len(sniffed_data) >= 100:
+			break
+
+	data_types = {col: sniff_type(sniff[col]) for col in columns}
+
+	declare_table(cur, data_types, table_name)
+
+	insert_command = 'INSERT INTO %s (%s) VALUES (%s);' % (
 		table_name,
 		','.join(columns),
-		','.join(['?']*len(columns))
+		','.join(['CAST(? AS %s)' % data_types[col] for col in columns])
 	)
-	cur.executemany(insert_string, data_list)
+
+	# TODO: double and triple check that we don't lose any records here.
+	records = itertools.chain(sniffed_data, csv_reader)
+
+	batch = True
+	while batch:
+		batch = list(itertools.islice(records, 10000))
+		cur.executemany(insert_command, batch)
+		sys.stdout.write('.')
+		sys.stdout.flush()
 	conn.commit()
+	sys.stdout.write('\n')
 
 
-for myfile in datafiles:
-	theZipper = zipfile.ZipFile(path+myfile)
-	datafiles = find_dat_files(theZipper.namelist())
-	for aDataFile in datafiles:
-		print aDataFile
-		rawData = []
-		for line in csv.reader(theZipper.open(aDataFile)):
-			rawData.append(line)
-		cols = parse_data(rawData)
-		table_name = aDataFile.split('.')[0]
-		drop_string = 'DROP TABLE IF EXISTS ' + table_name 
-		cur.execute(drop_string)
-		create_string = create_table_string(cols, table_name)
-		create_table(create_string, table_name)
-		insert_data(rawData, table_name)
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Locate input files')
+	parser.add_argument('-z', '--zipfile-location', dest='path',
+		default='~/Dropbox/ATUS-195W/data/test_zip/',
+		help='The location of the zipfiles with the ATUS data')
+	args = parser.parse_args()
+	arg_dict = vars(args)
 
+	path = os.path.expanduser(arg_dict['path'])
 
+	conn = sqlite3.connect('atus.db')
+	cur = conn.cursor()
+
+	for zip_name in os.listdir(path):
+		zip_handle = zipfile.ZipFile(os.path.join(path, zip_name))
+
+		datafiles = [fname for fname in zip_handle.namelist() if fname.endswith('.dat')]
+		for datafile in datafiles:
+			print 'Processing', datafile
+			table_name = datafile.split('.')[0]
+
+			build_table(
+				cur,
+				conn,
+				table_name,
+				csv.reader(zip_handle.open(datafile))
+			)
