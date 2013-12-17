8b9b7ff35548988450002e42a1d3e0b1391d161b
Sam Birch <sam.m.birch@gmail.com>
2013-12-17 14:27:21 -0500

    miscellania

--- api_test.py
+++ api_test.py
@@ -1,7 +1,37 @@
-import dataset
+import atus
 import variables
+from collections import namedtuple
+import sys
 
-db = ATUS(dataset.connect('sqlite:///db/atus.db'))
+'''
+count API
+filters (incl activity)
+normalization
+Output: person-minutes + bounds, record count, caseids
+How to get uncertainty on normalized proportions from uncertain part/whole?
+Evaluate confidence bounds by estimating state populations
+'''
+
+Count = namedtuple('Count', ['minutes', 'variance', 'interval', 'cases'])
+
+def count(alpha=0.95, **kwargs):
+	'''Count the number of respondents matching a given set of filters; returns
+	a Count object.'''
+
+	filters = '1=1'
+	if kwargs.has_key('filters'):
+		filters = kwargs['filters']
+	
+	results = atus.db.query('EXPLAIN SELECT count(*) FROM respondents inner join roster on respondents.caseid=roster.caseid and roster.lineno=1 inner join cps on cps.caseid=roster.caseid and cps.lineno=1 where %s' % filters)
+
+	for r in results:
+		print r
+		break
+
+if __name__ == '__main__':
+	count(filters='sex_code=2 and age=21')
+	sys.exit(0)
+
 
 def weighted_groupby_count():
 	'''
--- casedump.py
+++ casedump.py
@@ -5,9 +5,9 @@ caseid = int(sys.argv[1])
 
 print 'Case ID:', caseid
 
-demographics = atus.db.get('select age, sex_code from roster where case_id=%s and lineno=1' % caseid)
-demographics.update(atus.db.get('select region_code, race_code from cps where case_id=%s and lineno=1' % caseid))
-demographics.update(atus.db.get('select labor_status_code from respondents where case_id=%s' % caseid))
+demographics = atus.db.get('select age, sex_code from roster where caseid=%s and lineno=1' % caseid)
+demographics.update(atus.db.get('select region_code, race_code from cps where caseid=%s and lineno=1' % caseid))
+demographics.update(atus.db.get('select labor_status_code from respondents where caseid=%s' % caseid))
 
 print '%dyo %r %s from the %s who is %r' % (
 	demographics['age'],
@@ -19,6 +19,6 @@ print '%dyo %r %s from the %s who is %r' % (
 
 print 'Records:'
 for table in atus.db.tables:
-	results = list(atus.db.query('select * from %s where TUCASEID=%s' % (table, caseid)))
+	results = list(atus.db.query('select * from %s where caseid=%s' % (table, caseid)))
 	if results:
 		print '\t', table, 'has', len(results), 'records'
