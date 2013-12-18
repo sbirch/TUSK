61669ad4032df3bb0049951324ae8471d2ec2102
Sam Birch <sam.m.birch@gmail.com>
2013-12-18 02:55:54 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tusk',
+      version='0.1',
+      description='Time Use Survey Toolkit',
+      url='http://github.com/sbirch/cs195-atus',
+      author='Sam Birch, Alex Leblang',
+      author_email='sbirch@cs.brown.edu',
+      license='MIT',
+      packages=['tusk'],
+      zip_safe=False)
