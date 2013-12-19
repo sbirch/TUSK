4d49bbecb2a7fef73544ec6e47df8923214a38c3
Sam Birch <sam.m.birch@gmail.com>
2013-12-19 00:41:54 -0500

    household child analysis

--- tusk/analysis/analyses.py
+++ tusk/analysis/analyses.py
@@ -1,6 +1,13 @@
 from tusk import atus, variables
+import matplotlib.pyplot as plt
 
-results = atus.db.query('select ...')
+ages = []
+ft = []
+for r in atus.db.query('''SELECT
+	age, weighted_avg(family_time, respondents.weight)
+	FROM respondent_link(respondents, summary, cps) where PRNMCHLD=0 GROUP BY age;'''):
+	ages.append(r['age'])
+	ft.append(r['TUFNWGTP)'])
+plt.plot(ages, ft)
 
-for result_dict in results:
-	pass
+plt.show()
